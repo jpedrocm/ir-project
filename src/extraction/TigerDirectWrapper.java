f5f4900e0a8794fb209262b039f70bdf6ec64869
jpedrocm <jpcm@cin.ufpe.br>
2016-09-07 01:08:59 -0300

    Created wrapper for TigerDirect

---
+++ src/extraction/TigerDirectWrapper.java
@@ -0,0 +1,49 @@
+package extraction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+public class TigerDirectWrapper extends AbstractWrapper {
+
+	@Override
+	public String getProductName(Document doc) {
+		Element product = doc.getElementsByClass("prodName").get(0);
+		String productName = product.getElementsByTag("h1").get(0).text();
+		return productName;
+	}
+
+	@Override
+	public HashMap<String, List<String>> getSpecifications(Document doc) {
+		HashMap<String, List<String>> specifications = new HashMap<>();
+		
+		Element specTable = doc.getElementsByClass("prodSpec").get(0);
+		Elements specRows = specTable.getElementsByTag("tr");
+		
+		for(Element specRow : specRows){
+			if(specRow.children().size() > 1){
+				String specName = specRow.child(0).text();
+				String specValue = specRow.child(1).text();
+				List<String> valueList = new ArrayList<String>();
+				valueList.add(specValue);
+				specifications.put(specName, valueList);
+			}
+		}
+		
+		return specifications;
+	}
+
+	@Override
+	public String getPrice(Document doc) {
+		String price = doc.getElementsByClass("salePrice").get(0).text().trim();
+        StringBuilder priceBuilder = new StringBuilder(price);
+        priceBuilder.setCharAt(price.length()-1,' ');
+        String productPrice = priceBuilder.toString().trim();
+		return productPrice;
+	}
+}
