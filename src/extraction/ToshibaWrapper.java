105593907e2b8d0de626fa8eb45684870485d862
lfn2 <lfn2@cin.ufpe.br>
2016-10-14 18:37:11 -0300

    Fail-proof ToshibaWrapper

--- src/extraction/ToshibaWrapper.java
+++ src/extraction/ToshibaWrapper.java
@@ -5,27 +5,30 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ToshibaWrapper extends AbstractWrapper{
 
 	@Override
 	public String getProductName(Document doc) {
-		String productName = doc.getElementsByClass("sku-title").get(0).text();
-		return productName;
+	    String name = null;
+	    
+		Element productNameElement = doc.getElementsByClass("sku-title").first();
+		if (productNameElement != null)
+		    name = productNameElement.text();
+		
+		return name;
 	}
 
 	@Override
 	public HashMap<String, List<String>> getSpecifications(Document doc) {
-		HashMap<String, List<String>> specifications = new HashMap<String, List<String>>();
-		
-		specifications.put("Name", Arrays.asList(getProductName(doc)));
-        specifications.put("Price", Arrays.asList(getPrice(doc)));
+		HashMap<String, List<String>> specifications = super.getSpecifications(doc);
 		
 		Elements specNames = doc.getElementsByTag("dt");
 		Elements specValues = doc.getElementsByTag("dd");
 		
-		for(int i = 0; i < specNames.size(); i++){
+		for(int i = 0; i < specNames.size() && i < specValues.size(); i++){
 			String specName = specNames.get(i).text();
 			String specValue = specValues.get(i).text();
 			specifications.put(specName, Arrays.asList(specValue));
@@ -36,7 +39,15 @@ public class ToshibaWrapper extends AbstractWrapper{
 
 	@Override
 	public String getPrice(Document doc) {
-		String productPrice = doc.getElementById("sku-price").text();
-		return productPrice;
+	    String price = null;
+	    
+		Element productPriceElement = doc.getElementById("sku-price");
+		if (productPriceElement != null)
+		    price = productPriceElement.text();
+		
+		return price;
 	}
 }
+
+
+
